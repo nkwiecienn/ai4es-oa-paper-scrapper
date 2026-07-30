f3fb04dcc98cc526b4fe5e12aff84728b7278f9b
nkwiecienn <natalia.kwiecien.nk@gmail.com>
2026-07-30 14:44:58 +0200

    deleted old backend and frontend

--- frontend/src/app/sections/project-description/project-description.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-project-description',
-  imports: [],
-  templateUrl: './project-description.html',
-  styleUrl: './project-description.scss',
-})
-export class ProjectDescription {}
